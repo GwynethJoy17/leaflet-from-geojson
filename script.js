330373fc95302ae31cbbed23133316bb1fe1ba09
GwynethJoy17 <78654856+GwynethJoy17@users.noreply.github.com>
2022-02-20 20:47:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,276 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext
 	ext: 'png'
 }).addTo(map);
 
-var marker = L.marker([38.6450677978642, -90.26170132181247]).addTo(map);
-marker.bindPopup("Drunken Fish - Central West End").openPopup();
+var marker = L.marker([38.55398240472064, -90.31757109368486]).addTo(map);
+marker.bindPopup("AF1").openPopup();
 
-var marker = L.marker([38.62990709219426, -90.19214815573241]).addTo(map);
-marker.bindPopup("Sauce on the Side").openPopup();
+var marker = L.marker([38.56473456007476, -90.3334974489339]).addTo(map);
+marker.bindPopup("AF2").openPopup();
 
-var marker = L.marker([38.60842617075752, -90.20984355439138]).addTo(map);
-marker.bindPopup("John D. McGurks Irish Pub and Garden").openPopup();
+var marker = L.marker([38.55916071471179, -90.3007834324193]).addTo(map);
+marker.bindPopup("AF3").openPopup();
 
-var marker = L.marker([38.60132854215099, -90.24214614446257]).addTo(map);
-marker.bindPopup("Rooster").openPopup();
+var marker = L.marker([38.82382302929532, -90.25525437881771]).addTo(map);
+marker.bindPopup("BJ3").openPopup();
 
-var marker = L.marker([38.63251291876637, -90.19925077733251]).addTo(map);
-marker.bindPopup("Medina Mediterranean Grill").openPopup();
+var marker = L.marker([38.852221485261026, -90.29401037458808]).addTo(map);
+marker.bindPopup("BJ2").openPopup();
+
+var marker = L.marker([38.7224302497129, -90.33488147049907]).addTo(map);
+marker.bindPopup("BK2").openPopup();
+
+var marker = L.marker([38.617854229557445, -90.3477598599409]).addTo(map);
+marker.bindPopup("BW1").openPopup();
+
+var marker = L.marker([38.6404784081293, -90.56458452326592]).addTo(map);
+marker.bindPopup("MF1").openPopup();
+
+var marker = L.marker([38.64302321051031, -90.67314742759477]).addTo(map);
+marker.bindPopup("MF2").openPopup();
+
+var marker = L.marker([38.68345583014222, -90.48087353681893]).addTo(map);
+marker.bindPopup("MF3").openPopup();
+
+var marker = L.marker([38.681641941633494, -90.50673885356467]).addTo(map);
+marker.bindPopup("MF").openPopup();
+
+var marker = L.marker([38.665401146739505, -90.61828766255614]).addTo(map);
+marker.bindPopup("MF5").openPopup();
+
+var marker = L.marker([38.710781370142655, -90.34889853839627]).addTo(map);
+marker.bindPopup("CM1").openPopup();
+
+var marker = L.marker([38.70215976999943, -90.362966677184]).addTo(map);
+marker.bindPopup("CM2").openPopup();
+
+var marker = L.marker([38.725929465417465, -90.3732530478829]).addTo(map);
+marker.bindPopup("CM3").openPopup();
+
+var marker = L.marker([38.67258675072413, -90.42874879718929]).addTo(map);
+marker.bindPopup("CC").openPopup();
+
+var marker = L.marker([38.670274640457635, -90.44150042956359]).addTo(map);
+marker.bindPopup("CC1").openPopup();
+
+var marker = L.marker([38.51980847160586, -90.45289887375434]).addTo(map);
+marker.bindPopup("FN1").openPopup();
+
+var marker = L.marker([38.538863075907514, -90.47286523245701]).addTo(map);
+marker.bindPopup("FN2").openPopup();
+
+var marker = L.marker([38.523316168411846, -90.48717463354157]).addTo(map);
+marker.bindPopup("FN3").openPopup();
+
+var marker = L.marker([38.552040309030424, -90.41218133074084]).addTo(map);
+marker.bindPopup("FN4").openPopup();
+
+var marker = L.marker([38.741609422923325, -90.30335782691883]).addTo(map);
+marker.bindPopup("FG1").openPopup();
+
+var marker = L.marker([38.76410277161125, -90.28063021975164]).addTo(map);
+marker.bindPopup("FG2").openPopup();
+
+var marker = L.marker([38.784190776989874, -90.28881371993633]).addTo(map);
+marker.bindPopup("FV1").openPopup();
+
+var marker = L.marker([38.82004600865033, -90.34104213321108]).addTo(map);
+marker.bindPopup("FV2").openPopup();
+
+var marker = L.marker([38.634167853364694, -90.41209637135161]).addTo(map);
+marker.bindPopup("FV3").openPopup();
+
+var marker = L.marker([38.788172199364446, -90.3746240455634]).addTo(map);
+marker.bindPopup("FR1").openPopup();
+
+var marker = L.marker([38.73743229864227, -90.32388355915735]).addTo(map);
+marker.bindPopup("HW2").openPopup();
+
+var marker = L.marker([38.58176508002449, -90.40812620752936]).addTo(map);
+marker.bindPopup("KN1").openPopup();
+
+var marker = L.marker([38.56672649134661, -90.42765675736679]).addTo(map);
+marker.bindPopup("KW1").openPopup();
+
+var marker = L.marker([38.58977010703763, -90.43072137021808]).addTo(map);
+marker.bindPopup("KW2").openPopup();
+
+var marker = L.marker([38.64171231219375, -90.36756167284226]).addTo(map);
+marker.bindPopup("KW3").openPopup();
+
+var marker = L.marker([38.63761439797111, -90.39296506138645]).addTo(map);
+marker.bindPopup("LD1").openPopup();
+
+var marker = L.marker([38.53251851105162, -90.28711796145423]).addTo(map);
+marker.bindPopup("LD2").openPopup();
+
+var marker = L.marker([38.613414741433374, -90.32590553526647]).addTo(map);
+marker.bindPopup("LM1").openPopup();
+
+var marker = L.marker([38.71265296129832, -90.43135972395693]).addTo(map);
+marker.bindPopup("MD1").openPopup();
+
+var marker = L.marker([38.714235079960744, -90.46193100782293]).addTo(map);
+marker.bindPopup("MH1").openPopup();
+
+var marker = L.marker([38.5139577441876, -90.31736974518998]).addTo(map);
+marker.bindPopup("MH2").openPopup();
+
+var marker = L.marker([38.47499447628222, -90.30379513300176]).addTo(map);
+marker.bindPopup("ML1").openPopup();
+
+var marker = L.marker([38.52889526949473, -90.3779984608316]).addTo(map);
+marker.bindPopup("ML2").openPopup();
+
+var marker = L.marker([38.489762624310806, -90.38338075865622]).addTo(map);
+marker.bindPopup("ML3").openPopup();
+
+var marker = L.marker([38.523532435667285, -90.34508784562779]).addTo(map);
+marker.bindPopup("ML4").openPopup();
+
+var marker = L.marker([38.43847435684182, -90.32377333455102]).addTo(map);
+marker.bindPopup("ML5").openPopup();
+
+var marker = L.marker([38.481272563579246, -90.35475225916817]).addTo(map);
+marker.bindPopup("ML6").openPopup();
+
+var marker = L.marker([38.59368100718755, -90.54163537377032]).addTo(map);
+marker.bindPopup("ML7").openPopup();
+
+var marker = L.marker([38.55493871296345, -90.55188389575626]).addTo(map);
+marker.bindPopup("MW1").openPopup();
+
+var marker = L.marker([38.57793000579019, -90.64365551868634]).addTo(map);
+marker.bindPopup("MW2").openPopup();
+
+var marker = L.marker([38.60475146741851, -90.59380361639363]).addTo(map);
+marker.bindPopup("MW3").openPopup();
+
+var marker = L.marker([38.55574940533173, -90.69687924461033]).addTo(map);
+marker.bindPopup("MW4").openPopup();
+
+var marker = L.marker([38.685559831176256, -90.31323373090402]).addTo(map);
+marker.bindPopup("MW5").openPopup();
+
+var marker = L.marker([38.74989026721853, -90.26923987584209]).addTo(map);
+marker.bindPopup("MC1").openPopup();
+
+var marker = L.marker([38.74994884530454, -90.26918623124752]).addTo(map);
+marker.bindPopup("MN1").openPopup();
+
+var marker = L.marker([38.67450692566603, -90.38021458921463]).addTo(map);
+marker.bindPopup("OL1").openPopup();
+
+var marker = L.marker([38.743477657113324, -90.40912044797578]).addTo(map);
+marker.bindPopup("PB1").openPopup();
+
+var marker = L.marker([38.747804372468316, -90.43314966401397]).addTo(map);
+marker.bindPopup("PB2").openPopup();
+
+var marker = L.marker([38.77872334925204, -90.46080956318427]).addTo(map);
+marker.bindPopup("PB4").openPopup();
+
+var marker = L.marker([38.62833101639582, -90.31886080419659]).addTo(map);
+marker.bindPopup("RH1").openPopup();
+
+var marker = L.marker([38.74755631628808, -90.21134400080301]).addTo(map);
+marker.bindPopup("RV1").openPopup();
+
+var marker = L.marker([38.77761391087462, -90.40505147336047]).addTo(map);
+marker.bindPopup("RB1").openPopup();
+
+var marker = L.marker([38.593673571784215, -90.32630891685241]).addTo(map);
+marker.bindPopup("SH1").openPopup();
+
+var marker = L.marker([38.79525389882209, -90.21307316275747]).addTo(map);
+marker.bindPopup("SP1").openPopup();
+
+var marker = L.marker([38.78407764499829, -90.20018751626846]).addTo(map);
+marker.bindPopup("SP2").openPopup();
+
+var marker = L.marker([38.55318170230756, -90.47695832570103]).addTo(map);
+marker.bindPopup("VP1").openPopup();
+
+
+var marker = L.marker([38.56367126342382, -90.49912788918363]).addTo(map);
+marker.bindPopup("VP2").openPopup();
+
+var marker = L.marker([38.595047311951255, -90.51654732992027]).addTo(map);
+marker.bindPopup("WC1").openPopup();
+
+var marker = L.marker([38.5960267186847, -90.4817704172225]).addTo(map);
+marker.bindPopup("WC2").openPopup();
+
+var marker = L.marker([38.703734705319604, -90.39771501542573]).addTo(map);
+marker.bindPopup("WO1").openPopup();
+
+var marker = L.marker([38.4929836530213, -90.6307700989333]).addTo(map);
+marker.bindPopup("EK1").openPopup();
+
+var marker = L.marker([38.50554733007571, -90.66858985929264]).addTo(map);
+marker.bindPopup("EK2").openPopup();
+
+var marker = L.marker([38.77238771096017, -90.43492160197223]).addTo(map);
+marker.bindPopup("RB2").openPopup();
+
+var marker = L.marker([38.794695421460425, -90.266884343031313]).addTo(map);
+marker.bindPopup("BJ1").openPopup();
+
+var marker = L.marker([38.66793084793638, -90.5352886859466]).addTo(map);
+marker.bindPopup("MF4").openPopup();
+
+var marker = L.marker([38.700281606937146, -90.29104393116279]).addTo(map);
+marker.bindPopup("NE").openPopup();
+
+var marker = L.marker([38.74400188768331, -90.46722978568987]).addTo(map);
+marker.bindPopup("PB3").openPopup();
+
+var marker = L.marker([38.62795811798158, -90.48169500826803]).addTo(map);
+marker.bindPopup("WC3").openPopup();
+
+var marker = L.marker([38.771464622516746, -90.36326698640336]).addTo(map);
+marker.bindPopup("HW1").openPopup();
+
+var marker = L.marker([38.75055533326341, -90.32934620746107]).addTo(map);
+marker.bindPopup("BK1").openPopup();
+
+var marker = L.marker([38.6506299481681, -90.33615200001537]).addTo(map);
+marker.bindPopup("CL1").openPopup();
+
+var marker = L.marker([38.55891161872191, -90.38606086042923]).addTo(map);
+marker.bindPopup("CW1").openPopup();
+
+var marker = L.marker([38.58951665285995, -90.38636705753842]).addTo(map);
+marker.bindPopup("GD1").openPopup();
+
+var marker = L.marker([38.704103592141635, -90.29433445136232]).addTo(map);
+marker.bindPopup("NE1").openPopup();
+
+var marker = L.marker([38.73088065819814, -90.22838323298242]).addTo(map);
+marker.bindPopup("RV2").openPopup();
+
+var marker = L.marker([38.72126747756004, -90.26663727502816]).addTo(map);
+marker.bindPopup("RV3").openPopup();
+
+var marker = L.marker([38.6675212482583, -90.33670335822379]).addTo(map);
+marker.bindPopup("UC2").openPopup();
+
+var marker = L.marker([38.661711887720045, -90.30382730888542]).addTo(map);
+marker.bindPopup("UC1").openPopup();
+
+var marker = L.marker([38.59237031003373, -90.35651693101491]).addTo(map);
+marker.bindPopup("WG1").openPopup();
+
+var marker = L.marker([38.57085383786394, -90.35830778958977]).addTo(map);
+marker.bindPopup("WG2").openPopup();
+
+var marker = L.marker([38.68412664096275, -90.4063709217284]).addTo(map);
+marker.bindPopup("CC2").openPopup();
+
+var marker = L.marker([38.60144125537755, -90.44345333970604]).addTo(map);
+marker.bindPopup("DP1").openPopup();
+
+var marker = L.marker([38.79649743032255, -90.33120724201846]).addTo(map);
+marker.bindPopup("FV1").openPopup();
+
+var marker = L.marker([38.607329055301065, -90.36436550316115]).addTo(map);
+marker.bindPopup("RH1").openPopup();
